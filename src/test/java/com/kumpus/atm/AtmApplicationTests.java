833aae7408941c7210f4dd9de8a893c02ebac77e
ekaryagin <ekaryagin@avaya.com>
2024-04-16 22:25:07 +0200

    last test added

--- src/test/java/com/kumpus/atm/AtmApplicationTests.java
+++ src/test/java/com/kumpus/atm/AtmApplicationTests.java
@@ -1,14 +1,47 @@
 package com.kumpus.atm;
 
+import com.kumpus.atm.model.OperationResult;
+import com.kumpus.atm.service.ATM;
+import com.kumpus.atm.service.CashService;
+import com.kumpus.atm.service.IOService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.ByteArrayOutputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class AtmApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// TODO интеграционные тесты
-	}
+    @Autowired
+    private ATM atm;
 
+    @MockBean
+    private CashService cashService;
+
+    @MockBean
+    private IOService ioService;
+
+    @Test
+    void testRun() {
+        when(ioService.inputText()).thenReturn("exit");
+        when(cashService.executeCommand("exit")).thenReturn(OperationResult.success());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        doAnswer(invocation ->  {
+                String arg = invocation.getArgument(0, String.class);
+                outputStream.write(arg.getBytes());
+                return null;
+        }).when(ioService).outputText(anyString());
+
+        atm.run();
+
+        assertEquals("Your command:", outputStream.toString().trim());
+    }
 }
